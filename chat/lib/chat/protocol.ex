fc512808d3b9e71e0b5a9a2c357ac4d32ddfe87c
Jamie Curle <jamie@curle.io>
2026-05-11 21:26:20 +0100

    Added register and broadcasting enocode testcases

--- chat/lib/chat/protocol.ex
+++ chat/lib/chat/protocol.ex
@@ -35,5 +35,18 @@ defmodule Chat.Protocol do
   defp decode_broadcast(<<_::binary>>), do: :incomplete
 
   # page 43
-  # @spec encode_message
+  @spec encode_message(message()) :: iodata()
+  def encode_message(message)
+
+  def encode_message(%Register{} = msg) do
+    [0x01, encode_string(msg.username)]
+  end
+
+  def encode_message(%Broadcast{} = msg) do
+    [0x02, encode_string(msg.from_username), encode_string(msg.contents)]
+  end
+
+  def encode_string(str) do
+    [<<byte_size(str)::16>>, str]
+  end
 end
